3f7b900a2c7da6de06465d115bbdcb4e16cf150e
Tanjeer Bin Haque <tbh.tilok@live.com>
2022-01-15 21:16:38 +0600

    committed with dashboard list page implementing

--- src/app/ambs-reporting-app/src/environments/environment.prod.ts
+++ src/app/ambs-reporting-app/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import { HttpHeaders } from "@angular/common/http";
+
 export const environment = {
-  production: true
+  production: true,
+  apiEndPoint: 'https://localhost:7078/api/v1.0/',
+  header: new HttpHeaders().set("Access-Control-Allow-Origin", "*")
 };
