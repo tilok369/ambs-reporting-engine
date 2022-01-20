8246a505a1f842cd62d5f9d0804d3280f860217e
Shihab Uddin <surashel891@gmail.com>
2022-01-20 17:27:09 +0600

    dashboard.component.ts

--- src/app/ambs-reporting-app/src/app/components/dashboard/dashboard.component.ts
+++ src/app/ambs-reporting-app/src/app/components/dashboard/dashboard.component.ts
@@ -16,8 +16,8 @@ export class DashboardComponent implements OnInit {
     , private _reportService: ReportService) { }
 
   ngOnInit(): void {
-    //this.renderGraph("chart-container-1", 6, '%40EndDate%232021-01-31%7C%40StateId%23-1%7C%40ZoneId%234%7C%40DistrictId%234%7C%40RegionId%2327%7C%40BranchId%2333');
-    //this.renderGraph("chart-container-2", 5, '%40EndDate%232021-01-31%7C%40StateId%23-1%7C%40ZoneId%234%7C%40DistrictId%234%7C%40RegionId%2327%7C%40BranchId%2333');
+    this.renderGraph("chart-container-1", 6, '%40EndDate%232021-01-31%7C%40StateId%23-1%7C%40ZoneId%234%7C%40DistrictId%234%7C%40RegionId%2327%7C%40BranchId%2333');
+    this.renderGraph("chart-container-2", 5, '%40EndDate%232021-01-31%7C%40StateId%23-1%7C%40ZoneId%234%7C%40DistrictId%234%7C%40RegionId%2327%7C%40BranchId%2333');
     //this.renderGraph("chart-container-2", 5, '@EndDate#2021-01-31|@StateId#-1|@ZoneId#4|@DistrictId#4|@RegionId#27|@BranchId#33');
     this.getExportReportData(11, "%40BranchId%232%7C%40Date%232021-09-02");
     this.getExportReportDataLoanDisburseAndFullPayment(12, "%40BranchId%232%7C%40Date%232021-09-02");
