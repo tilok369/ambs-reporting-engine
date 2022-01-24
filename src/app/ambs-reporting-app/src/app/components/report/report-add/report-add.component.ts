b1a63b1b545ec69ae1f2fc9d3454108ecba56714
Mr. Shihab Uddin <shihab.bd@asa-international.com>
2022-01-24 13:07:52 +0600

    Report filter with move

--- src/app/ambs-reporting-app/src/app/components/report/report-add/report-add.component.ts
+++ src/app/ambs-reporting-app/src/app/components/report/report-add/report-add.component.ts
@@ -38,9 +38,9 @@ export class ReportAddComponent implements OnInit {
     this._filterService.getFilters(1, 50).subscribe((res: any) => {
       console.log(res);
       this.filterList = res;
-      this.filterList.unshift({ id: 0, name: '' });
+      //this.filterList.unshift({ id: 0, name: '' });
       this.deSelectedFilterList = [...this.filterList];
-      this.selectedFilterList.unshift({ id: 0, name: '' });
+      //this.selectedFilterList.unshift({ id: 0, name: '' });
     })
   }
   getGraphTypes() {
@@ -49,44 +49,68 @@ export class ReportAddComponent implements OnInit {
     })
   }
   selectFilter(selectedValue: any) {
-    if (selectedValue.value == 0) return;
-    this.deSelectedFilterList = this.deSelectedFilterList.filter(f => f.id != selectedValue.value);
-    this.selectedFilterList = this.filterList.filter((objFromA) => {
-      return !this.deSelectedFilterList.find(function (objFromB) {
-        return objFromA.id === objFromB.id
-      })
-    })
-    if (!this.selectedFilterList.find(f => f.id == 0)) this.selectedFilterList.unshift({ id: 0, name: '' });
+    if (!selectedValue) return;
+    this.deSelectedFilterList = this.deSelectedFilterList.filter(f => f.id != selectedValue.id);
+    // this.selectedFilterList = this.filterList.filter((objFromA) => {
+    //   return !this.deSelectedFilterList.find(function (objFromB) {
+    //     return objFromA.id === objFromB.id
+    //   })
+    // })
+    // if (!this.selectedFilterList.find(f => f.id == 0)) this.selectedFilterList.unshift({ id: 0, name: '' });
+    this.selectedFilterList.push(selectedValue);
   }
   deSelectFilter(deSelectedValue: any) {
-    if (deSelectedValue.value == 0) return;
-    this.selectedFilterList = this.selectedFilterList.filter(f => f.id != deSelectedValue.value);
-    this.deSelectedFilterList = this.filterList.filter((objFromA) => {
-      return !this.selectedFilterList.find(function (objFromB) {
-        return objFromA.id === objFromB.id
-      })
-    })
-    if (!this.deSelectedFilterList.find(f => f.id == 0)) this.deSelectedFilterList.unshift({ id: 0, name: '' });
+    if (!deSelectedValue) return;
+    this.selectedFilterList = this.selectedFilterList.filter(f => f.id != deSelectedValue.id);
+    this.deSelectedFilterList.push(deSelectedValue);
+    // if (deSelectedValue.value == 0) return;
+    // this.selectedFilterList = this.selectedFilterList.filter(f => f.id != deSelectedValue.value);
+    // this.deSelectedFilterList = this.filterList.filter((objFromA) => {
+    //   return !this.selectedFilterList.find(function (objFromB) {
+    //     return objFromA.id === objFromB.id
+    //   })
+    // })
+    // if (!this.deSelectedFilterList.find(f => f.id == 0)) this.deSelectedFilterList.unshift({ id: 0, name: '' });
   }
+  downPosition(filter: any) {
+    let currentIndex = this.selectedFilterList.indexOf(filter);
+    if (currentIndex === this.selectedFilterList.length - 1) return;
+    this.selectedFilterList=this.arrayMove(this.selectedFilterList,currentIndex,currentIndex+1);
+  }
+  upPosition(filter: any) {
+    let currentIndex = this.selectedFilterList.indexOf(filter);
+    if (currentIndex <1) return;
+    this.selectedFilterList=this.arrayMove(this.selectedFilterList,currentIndex,currentIndex-1);
+  }
+  arrayMove(arr:any, oldIndex:number, newIndex:number) {
+    if (newIndex >= arr.length) {
+      var k = newIndex - arr.length + 1;
+      while (k--) {
+        arr.push(undefined);
+      }
+    }
+    arr.splice(newIndex, 0, arr.splice(oldIndex, 1)[0]);
+    return arr; 
+  };
   public get reportType(): typeof ReportType {
     return ReportType;
   }
-  validate():string{
-    let errorMessage:string='';
-    if(!this.report.name)errorMessage+='Name is required<br>';
-      if(!this.report.type) errorMessage+='Type is required<br>';
-      if(this.report.type==ReportType.Tabular){
-       if(!this.report.tabularFeature.script) errorMessage+='Script is required<br>';
-      }else{
-        if(!this.report.graphicalFeature.script) errorMessage+='Script is required<br>';
-        if(!this.report.graphicalFeature.graphType) errorMessage+='Graph Type is required<br>';
-      }
+  validate(): string {
+    let errorMessage: string = '';
+    if (!this.report.name) errorMessage += 'Name is required<br>';
+    if (!this.report.type) errorMessage += 'Type is required<br>';
+    if (this.report.type == ReportType.Tabular) {
+      if (!this.report.tabularFeature.script) errorMessage += 'Script is required<br>';
+    } else {
+      if (!this.report.graphicalFeature.script) errorMessage += 'Script is required<br>';
+      if (!this.report.graphicalFeature.graphType) errorMessage += 'Graph Type is required<br>';
+    }
     return errorMessage;
   }
   saveReport() {
-    this.message=this.validate();
-    if(this.message.length>0)return;
-    this.report.reportFilterList=[];
+    this.message = this.validate();
+    if (this.message.length > 0) return;
+    this.report.reportFilterList = [];
     if (this.selectedFilterList.length > 0) {
       this.selectedFilterList.filter(f => f.id != 0).forEach((f, i) => {
         this.report.reportFilterList?.push({ id: 0, reportId: 0, filterId: f.id, sortOrder: i + 1 })
